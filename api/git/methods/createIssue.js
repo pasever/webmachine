780aa7f6ea9f489bb6fbf933ce9f2f7a322f13e5
jc <javascriptdeveloper17@gmail.com>
2018-03-22 19:43:32 -0400

    createIssue method works. not hooked up to client side yet

--- api/git/methods/createIssue.js
+++ api/git/methods/createIssue.js
@@ -12,4 +12,34 @@ github.authenticate({
   token: githubrepo.token
 });
 
-// console.log(github);
+const req = {
+  owner: 'strategicmarket',
+  repo: 'workitem-lab',
+  title: '',
+  body: ''
+};
+
+async function createIssue(req) {
+  let result = await github.issues.create(req)
+  .catch(err => { throw err });
+  let resObj = {
+    status: result.meta.status,
+    url: result.data.html_url
+  };
+  return resObj;
+}
+
+createIssue(req).then(res => console.log(res));
+
+module.exports = createIssue;
+
+// github.issues.create({
+//   owner: 'strategicmarket',
+//   repo: 'workitem-lab',
+//   title: 'first test',
+//   body: 'I am the first issue being created with the GitHub Node.js client!'
+// }).then(result => {
+//   console.log(result.data);
+// }).catch(error => {
+//   console.log(error);
+// })
