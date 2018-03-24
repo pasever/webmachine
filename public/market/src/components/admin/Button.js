23d776a4ec55974ead1de9e510865e50757b8bc4
jc <javascriptdeveloper17@gmail.com>
2018-03-24 15:38:25 -0400

    fixed bootstrap issues

--- public/market/src/components/admin/Button.js
+++ public/market/src/components/admin/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => (
   <button
       type="button"
-      className="btn btn-secondary"
+      className="btn btn-info"
       style={{display: 'block', margin: '0 auto'}}
       data-toggle="modal"
       data-target="#createWorkitem"
