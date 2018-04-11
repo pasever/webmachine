de15f9e60848ea4d40cec741d0eaee1d561fe252
jc <javascriptdeveloper17@gmail.com>
2018-04-11 19:44:40 -0400

    wip commit

--- public/platform/src/pages/LoadingPage.js
+++ public/platform/src/pages/LoadingPage.js
@@ -3,18 +3,17 @@
 ///////////////////////////////////////////////////////////////////////
 // DGO
 
-'use strict';
+"use strict";
 
-
-import React from 'react';
-import '../styles/loadingpage.css';
+import React from "react";
+import "../styles/loadingpage.css";
 
 const LoadingPage = () => (
-    <div className="loading-page-wrapper">
-        <p>
-            <i className="fa fa-gear fa-spin"></i>
-        </p>
-    </div>
+  <div className="loading-page-wrapper">
+    <p>
+      <i className="fa fa-gear fa-spin" />
+    </p>
+  </div>
 );
 
 export default LoadingPage;
--- public/platform/src/pages/Partials/CardSection.js
+++ public/platform/src/pages/Partials/CardSection.js
@@ -3,17 +3,16 @@
 ///////////////////////////////////////////////////////////////////////
 // DGO
 
-
-import React, { Component } from 'react';
-import {CardElement, PostalCodeElement} from 'react-stripe-elements';
-
+import React, { Component } from "react";
+import { CardElement, PostalCodeElement } from "react-stripe-elements";
 
 /// Displays the credit card enter fields from stripe
-export const CardSection = () => 
-    <div className="form-group">
-        <label>Card details</label>
-        <CardElement 
-            style={{ base: {fontSize: "18px" }}} 
-            className="form-control" />
-    </div>  
-
+export const CardSection = () => (
+  <div className="form-group">
+    <label>Card details</label>
+    <CardElement
+      style={{ base: { fontSize: "18px" } }}
+      className="form-control"
+    />
+  </div>
+);
