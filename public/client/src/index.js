c69b7c86d0475e291f998a46c4417c23156b733b
Juan Carlos Gonzalez <javascriptdeveloper17@gmail.com>
2018-05-08 10:23:20 -0400

    test push

--- public/client/src/index.js
+++ public/client/src/index.js
@@ -10,3 +10,9 @@ import App              from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 module.hot.accept();
+
+/* 
+    Hello Dan, if you see this you can delete this comment.
+    Checking whether I can successfully push to your fork.
+    - Juan 
+ */
