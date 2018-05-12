b17fe0a2fc1adfa652769989150d04ddc359ab26
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-05-12 13:29:23 -0400

    working on validation before submit

--- public/member/src/MemberRegistration.js
+++ public/member/src/MemberRegistration.js
@@ -31,24 +31,25 @@ class MemberRegistration extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Defaults to step1 of the registration process.
       location: 'networks-to-join'
     };
 
     this.liftChildState = this.liftChildState.bind(this);
   }
 
-  /** @method */
-  // Is passed through props to children as handler to lift state.
-  /** 
+  /** @method 
+   * @description - Is passed through props to children as handler to lift state.
    * @param {String} page - identifier of page user is navigating to.
-   * @param {Array|Object} load - data stored in the child's state.
+   * @param {Array|Object} load - data passed from the child's state.
    */
   liftChildState(page, load) {
     // replace any dashes (-) with underscores (_).
     // use the output as name of new property in state.
-    // use this.state.location to test & replace because the
-    // incoming load is for the current location, not the next one.
-    let loc = this.state.location;
+    // i.e. uses networks-to-join to save array (in state) called networks_to_join
+    // uses this.state.location to test & replace because the
+    // incoming load is from the current location, not the next one.
+    let loc = this.state.location === '' ? 'networks-to-join' : this.state.location;
     let stateProp = /-/.test(loc) ? loc.replace('-','_') : page;
     this.setState({ location: page, [stateProp]: load })
   }
@@ -56,11 +57,54 @@ class MemberRegistration extends Component {
   // Renders component based on page location
   renderPage() {
     let { location } = this.state;
+
     if (location === 'networks-to-join') {
       return <NetworkSelection liftState={this.liftChildState}  />
     } else if (location === 'member-form') {
       return <MemberForm liftState={this.liftChildState}  />
+    } else {
+      // If, for whatever reason, the value of location is wiped out
+      // from state, render the first step of the registration process.
+      return <NetworkSelection liftState={this.liftChildState}  />
     }
+  }
+
+  // Passed to the component handling last step of the process.
+  // Triggered when 'Complete Registration' button is clicked.
+  // Performs a few things:
+  // 1. Checks that load from each step IS NOT empty (@method)
+  //  1.1 If one or more is empty, it DOESN'T submit
+  //  1.2 If none are empty, it moves on.
+  // 2. Makes request to member registration route in backend,
+  //    effectively passing ALL information necessary to register
+  //    a new member.
+  // 3. Waits for response and notifies user of outcome.
+  registerMember() {
+
+    function loadHasEmptyFields() {};
+    /** @todo */
+    // have a method to check if load is empty and another to check if
+    // load has any empty fields
+    function loadIsEmpty(load) {
+      // if it's not an array, then it's a plain object
+      if (Array.isArray(load)) {
+        if(load.length === 0)
+          alert('Please select at least one network to join');
+          return true;
+      } else {
+        // checks if it has empty fields! not if it's empty...
+        let loadValues = Object.values(load);
+        for (let i = 0; i < loadValues.length; i++) {
+          if (loadValues[i] === '') {
+            alert('Please fill out all fields in the form');
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }
+
   }
 
   render() {
