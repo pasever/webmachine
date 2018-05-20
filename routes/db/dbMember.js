6379e44d6662211fe4162916da92550b0c7033cd
jc <javascriptdeveloper17@gmail.com>
2018-05-20 18:50:11 -0400

    added middleware to protect route and now extracting id from token set in authorization header

--- routes/db/dbMember.js
+++ routes/db/dbMember.js
@@ -4,11 +4,16 @@
 //////// route for new member registration ///////
 //////////////////////////////////////////////////
 
-const bodyParser =  			require('body-parser')
-const api =         			require('../../api')
+const bodyParser          = require('body-parser')
+const api                 = require('../../api')
+const {
+  verifyJWTToken,
+  getIdFromToken
+}                         = require('../../utils/auth/verifyJwtToken')
 
 const dbMember = (router) => {
   /**
+   * @private {Route}
    * @method POST
    * @description
    * Receives member payload and calls on DB method to insert
@@ -17,11 +22,37 @@ const dbMember = (router) => {
 
   router.use(bodyParser.json())
 
-  router.post('/register', (req, res) => {
+  router.post('/register', verifyJWTToken, (req, res, next) => {
     console.log('got a request');
+    let memberPayload = req.body;
+    let idToken = getIdFromToken(req.headers.authorization);
+    console.log(idToken);
+    console.log(buildMemberDocument(memberPayload));
     res.json(req.body)
   });
 
 }
 
 module.exports = dbMember
+
+/**
+ * @param {Object}    - Receives member payload with information
+ *                      necessary to build Member Document.
+ * @returns {Object}  - Return a new instance of Member Schema
+ * 
+ * */
+function buildMemberDocument(memberPayload) {
+
+  return 'Got the payload of type ' + typeof memberPayload
+
+}
+
+function checkForIdToken(headers) {
+
+  function extractIdToken(headers) {}
+
+}
+
+async function fetchNetworksToJoinInfo() {}
+
+async function registerMemberToEachNetwork() {}
