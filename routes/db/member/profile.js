470cd42cf6028f208b4d3c07826529354a9ee854
DanOrlovsky <dangorlov@yahoo.com>
2018-05-28 22:15:38 -0400

    Fixed updating profile

--- routes/db/member/profile.js
+++ routes/db/member/profile.js
@@ -44,7 +44,7 @@ const memberProfile = (router) => {
   router.put('/:clientId', async (req, res, next) => {
     const { clientId } = req.params;
     const memberId = getIdFromToken(req.headers.authorization);
-    const { memberUpdates } = req.body;
+    const memberUpdates = req.body;
 
     // Fetch Client's URI
     const query1 = { "_id": clientId };
